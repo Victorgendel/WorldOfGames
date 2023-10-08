fd0056831034571c3d24c735f062639aabc697df
Victorgendel <victorgendel@gmail.com>
2023-10-08 19:45:06 +0300

    test e2e

--- e2e.py
+++ e2e.py
@@ -1,21 +1,20 @@
 from selenium import webdriver
 from time import sleep
 import sys
-# def test_scores_service(url):
-#     driver1 = webdriver.Firefox()
-#     driver1.get(url)
-#     web_score = driver1.find_element('id','score')
-#     print(web_score)
-#
+
 def test_scores_service():
-    driver1 = webdriver.Chrome()
-    driver1.get("http://127.0.0.1:5000/")
-    sleep(5)
-    login = driver1.find_element("xpath","/html/body/table/tbody/tr[2]/td[2]").text
-    print(login)
-    if 1 <= int(login) < 1000:
-        return True
-    else:
+    try:
+        driver1 = webdriver.Chrome()
+        driver1.get("http://127.0.0.1:5000/")
+        sleep(5)
+        login = driver1.find_element("xpath", "/html/body/table/tbody/tr[2]/td[2]").text
+        print(login)
+        if 1 <= int(login) < 1000:
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(f"There is no User Score yet please play a game: {e}")
         return False
 
 def main_function():
@@ -25,4 +24,5 @@ def main_function():
     else:
         print("Tests failed.")
         return sys.exit(-1)
+
 print(main_function())
